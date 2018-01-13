140141afb8b56192e8e188f37f3b57a49930263a
Wiktor <viktorius18@gmail.com>
2018-01-13 12:07:36 +0100

    hello task2 message

--- src/main/java/trening/Main.java
+++ src/main/java/trening/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from mater");
         System.out.println("Hello world");
+        System.out.println("hello task2");
     }
 }
