7bc5dfa8e151d5ccc54d1e158f741129d3e6c13d
Wiktor <viktorius18@gmail.com>
2018-01-13 13:54:12 +0100

    Commit
    This reverts commit 9916244

--- src/main/java/trening/Main.java
+++ src/main/java/trening/Main.java
@@ -3,7 +3,7 @@ package trening;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from mater");
-        System.out.println("Hello world 2");
+        System.out.println("Hello world");
 
     }
 }
