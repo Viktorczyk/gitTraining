7477860adbdb43b5b3362340df3cb1416caf5eff
Wiktor <viktorius18@gmail.com>
2018-01-13 12:18:34 +0100

    mastetr messege

--- src/main/java/trening/Main.java
+++ src/main/java/trening/Main.java
@@ -3,7 +3,6 @@ package trening;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from mater");
-        System.out.println("Hello world");
-        System.out.println("hello task2");
+
     }
 }
